d2e063a36abf4a976dc6886697db3a2efeb2d57f
--globa <sheikhamna754@gmail.com>
2022-07-01 11:19:09 +0500

    time left to live

--- lifeInweek.js
+++ lifeInweek.js
@@ -4,8 +4,8 @@ var yearRemaining = 90-age;
 var days =yearRemaining*365;
 var weeks =yearRemaining*52;
 var months =yearRemaining*12
-
-    console.log('you have '+ days + ' days '+ weeks +' weeks '+ months +' months left ')
- 
+let timeLeft = console.log('you have '+ days + ' days '+ weeks +' weeks '+ months +' months left ')
+return timeLeft;
 }
-lifeInWeek(78);
+var remainingTime=lifeInWeek(78);
+console.log(remainingTime)
