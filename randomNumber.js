1a6b247231d03278bce25d3fa720ac23e325b04d
--globa <sheikhamna754@gmail.com>
2022-07-02 15:13:21 +0500

    love calculator

---
+++ randomNumber.js
@@ -0,0 +1,9 @@
+prompt('your name');
+prompt('Youer parter number');
+var loveScore=(Math.random()*100)
+if(loveScore > 70){
+    alert(Math.round(loveScore)+'%'+'\n'+' you love each other like romeo julient' );
+}
+    else{
+        alert(Math.round(loveScore)+'%')
+    }
