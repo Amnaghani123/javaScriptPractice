3ac0b443ff1058613c8ffadc546870c3b2bf43e1
--globa <sheikhamna754@gmail.com>
2022-07-02 20:07:26 +0500

    inhancement

--- randomNumber.js
+++ randomNumber.js
@@ -4,6 +4,9 @@ var loveScore=(Math.random()*100)
 if(loveScore > 70){
     alert(Math.round(loveScore)+'%'+'\n'+' you love each other like romeo julient' );
 }
+if(loveScore >30 && loveScore <=70 ){
+    alert(Math.round(loveScore)+'%')
+}
     else{
-        alert(Math.round(loveScore)+'%')
+        alert(Math.round(loveScore)+'%' +'your love like oil and water')
     }
